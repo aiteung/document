f225ac08f315196402bf71d111d052be54c45d18
rofinafiin <rofinafiisr@gmail.com>
2023-12-20 16:19:32 +0700

    fix(types): function generator qr

---
+++ signature/qrgen.go
@@ -0,0 +1,43 @@
+package signature
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aiteung/document/client"
+	"github.com/aiteung/document/types"
+	"github.com/skip2/go-qrcode"
+)
+
+func CreateToken(docid, url string, data types.SignatureData) (token string) {
+	resp := new(types.TokenResp)
+	body := new(types.RequestData)
+
+	body.Id = docid
+	body.Data = data
+
+	res, err := client.CreateRequestHTTP().
+		SetBody(body).
+		Post(url)
+
+	if err != nil {
+		return "error ni kakak sistem akademiknya silahkan hubungi admin yaaaaa........"
+	}
+
+	defer res.Body.Close()
+	_ = json.Unmarshal(res.Bytes(), &resp)
+
+	token = resp.Token
+
+	return token
+}
+
+func CreateQRCode(link string, filename string) error {
+	// Generate QR code
+	err := qrcode.WriteFile(link, qrcode.Medium, 256, filename)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("QR code generated and saved to %s\n", filename)
+	return nil
+}
