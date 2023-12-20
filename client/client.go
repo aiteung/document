6b7d4523c6b830884e4c00a6066ab16c666b4973
rofinafiin <rofinafiisr@gmail.com>
2023-12-20 16:19:43 +0700

    fix(types): functions client

---
+++ client/client.go
@@ -0,0 +1,20 @@
+package client
+
+import (
+	"encoding/json"
+	"github.com/imroc/req/v3"
+)
+
+func CreateClientHTTP() *req.Client {
+	return req.
+		C().
+		SetJsonUnmarshal(json.Unmarshal).
+		SetJsonMarshal(json.Marshal)
+}
+
+func CreateRequestHTTP() *req.Request {
+	return CreateClientHTTP().
+		R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Accept-Language", "en-US,en;q=0.8,id;q=0.6")
+}
