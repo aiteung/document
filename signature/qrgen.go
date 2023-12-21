82f4c46fe651a40d125c4c49859d28951eb88375
rofinafiin <rofinafiisr@gmail.com>
2023-12-21 15:58:32 +0700

    fix(qr): ganti style qr ke highest

--- signature/qrgen.go
+++ signature/qrgen.go
@@ -33,7 +33,7 @@ func CreateToken(docid, url string, data types.SignatureData) (token string) {
 
 func CreateQRCode(link string, filename string) error {
 	// Generate QR code
-	err := qrcode.WriteFile(link, qrcode.Medium, 256, filename)
+	err := qrcode.WriteFile(link, qrcode.Highest, 256, filename)
 	if err != nil {
 		return err
 	}
