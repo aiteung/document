c78ab833e37dca97c10c41f91391cd565033a4b8
rofinafiin <rofinafiisr@gmail.com>
2024-02-06 09:40:52 +0700

    fix(sign): ubah model untuk data ttd dari npm menjadi pemilik document

--- types/types.go
+++ types/types.go
@@ -1,9 +1,9 @@
 package types
 
 type SignatureData struct {
-	PenandaTangan string `json:"penanda-tangan"`
-	DocName       string `json:"doc-name"`
-	NpmMahasiswa  string `json:"npm-mahasiswa"`
+	PenandaTangan   string `json:"penanda-tangan"`
+	DocName         string `json:"doc-name"`
+	PemilikDocument string `json:"pemilik_document"`
 }
 
 type RequestData struct {
